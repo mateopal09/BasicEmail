1c897806a241ee7b7977c9da76b078de59791aeb
Julián Orozco V <jorozcova@unal.edu.co>
2024-03-01 08:20:48 -0500

    Update api.ts

--- frontend/basicemail_front/src/api.ts
+++ frontend/basicemail_front/src/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const getEmails = (currentUser: string) => {
+/*export const getEmails = (currentUser: string) => {
     return axios.get('http://localhost:8000/api/recieve-email/' + currentUser,  {withCredentials: true});
 }
 
@@ -15,6 +15,23 @@ export const getUser = (email: string, password: string) => {
 
 export const postUser = (name: string, email: string, password: string, photo_profile:string) => {
     return axios.post('http://localhost:8000/api/register/', { "name": name, "email": email, "password": password, "photo_profile": photo_profile}, {withCredentials: true});
+}*/
+
+export const getEmails = (currentUser: string) => {
+    return axios.get('http://18.224.68.173:8000/api/recieve-email/' + currentUser,  {withCredentials: true});
+}
+
+export const postEmail = (subject: string, email: string, body: string) => {
+    console.log({ "recipient_email": email, "subject": subject, "body": body })
+    return axios.post('http://18.224.68.173:8000/api/send-email/', { "recipient_email": email, "subject": subject, "body": body },  {withCredentials: true});
+}
+
+export const getUser = (email: string, password: string) => {
+    return axios.post('http://18.224.68.173:8000/api/login/', { "email": email, "password": password }, {withCredentials: true});
+}
+
+export const postUser = (name: string, email: string, password: string, photo_profile:string) => {
+    return axios.post('http://18.224.68.173:8000/api/register/', { "name": name, "email": email, "password": password, "photo_profile": photo_profile}, {withCredentials: true});
 }
 
 export const getPicture = () => {
